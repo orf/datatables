0cd789faf1c9eaa49ed2f6c844b724a6e1065eb6
Savaş <sen.savas@gmail.com>
2016-04-11 13:00:30 +0300

    Support for -1 length type. (Means All records) See: https://datatables.net/reference/option/lengthMenu

--- datatables/__init__.py
+++ datatables/__init__.py
@@ -157,7 +157,9 @@ class DataTable(object):
             query = query.order_by(model_column.desc() if direction == "desc" else model_column.asc())
 
         filtered_records = query.count()
-        query = query.slice(start, start + length)
+
+        if length > 0:
+            query = query.slice(start, start + length)
 
         return {
             "draw": draw,
